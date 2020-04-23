606171e791345d704767ba273547cafb577597ed
line2nine <hailinh203@gmail.com>
2020-04-23 14:42:00 +0700

    Add files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+function isFirstLetterUpperCase($str) {
+    $regexp = '/^[A-Z]/';
+    if (preg_match($regexp, $str)) {
+        echo "Chữ cái đầu có viết hoa";
+    } else {
+        echo "Chữ cái đầu không viết hoa";
+    }
+}
+
+isFirstLetterUpperCase("Nguyen Van A");
+echo "<br>";
+isFirstLetterUpperCase("nguyen van a");
+echo "<br>";
+isFirstLetterUpperCase("NGUYEN VAN A");
